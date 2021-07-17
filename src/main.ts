d22a7f28fdd61867e10ccd8ff3afc82e70d1caf8
Gabriel Fonseca <gabriel.fonsecadev@gmail.com>
2021-07-17 17:15:31 -0300

    chore: fix ports

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService: ConfigService = app.get(ConfigService);
-  await app.listen(configService.get('server.port'));
+  await app.listen(configService.get('server.port') || 3000);
 }
 bootstrap();
